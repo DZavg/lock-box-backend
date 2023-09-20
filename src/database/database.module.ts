2d15981bdf7e1053f6ae171b4b6fff90eb42a5c2
Данил <d.zavgorodnij@meta.studio>
2023-09-20 19:27:38 +0700

    feat(database): change path to entities

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: ['dist/entities/**/*.entity.js'],
+        entities: ['dist/**/entities/*.entity.js'],
         synchronize: true,
       }),
     }),
