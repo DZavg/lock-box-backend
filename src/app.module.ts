8d2e83572e2dd49ab5b18dadcdbfe1fcde36715c
Данил <d.zavgorodnij@meta.studio>
2023-09-20 19:28:20 +0700

    feat(database): put validationSchema in a separate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,12 @@ import { AppService } from '@/app.service';
 import { DatabaseModule } from '@/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import ValidationSchema from '@/helpers/validationSchema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-      }),
+      validationSchema: Joi.object(ValidationSchema),
     }),
     DatabaseModule,
   ],
