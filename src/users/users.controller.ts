5f7cb3ac2f320171e375eba5eb572522ecfbcdbd
Данил <d.zavgorodnij@meta.studio>
2023-09-22 20:30:35 +0700

    feat(swagger): add tag in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,7 +16,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import PostgresErrorCodeEnum from '@/database/types/postgresErrorCode.enum';
 import { hashPassword } from '@/utils/password';
 import { UpdateUserDto } from '@/users/dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
