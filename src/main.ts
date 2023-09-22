05c0278aeacdc950be15f36bddf2a1d4abbe8525
Данил <d.zavgorodnij@meta.studio>
2023-09-22 19:15:26 +0700

    feat(exceptionFactory): change response for errors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      exceptionFactory: (errorsList) => {
+        const errors = {};
+        errorsList.forEach(function (error) {
+          errors[error.property] = Object.values(error.constraints);
+        });
+        return new BadRequestException({
+          errors,
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+      },
     }),
   );
   await app.listen(5001);
