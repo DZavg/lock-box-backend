2916af57d98543d0c2a9fcd2f93d37f080a0781f
Данил <d.zavgorodnij@meta.studio>
2023-09-22 19:14:41 +0700

    feat(errorMessage): create universal errors list

---
+++ src/utils/errorMessage.ts
@@ -0,0 +1,6 @@
+export const errorMessage = {
+  IsNotEmpty: 'Поле обязательно для заполнения',
+  IsString: 'Поле должно быть строкой',
+  IsEmail: 'Неверный формат почты',
+  Length: (min, max) => `Поле должно быть от ${min} до ${max} символов`,
+};
