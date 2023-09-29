fbb7530b97dd5fd542225f3e203cf2bcf63a6063
Данил <d.zavgorodnij@meta.studio>
2023-09-29 20:45:00 +0700

    feat(errorMessage): add new error messages

--- src/utils/errorMessage.ts
+++ src/utils/errorMessage.ts
@@ -2,5 +2,8 @@ export const errorMessage = {
   IsNotEmpty: 'Поле обязательно для заполнения',
   IsString: 'Поле должно быть строкой',
   IsEmail: 'Неверный формат почты',
+  ServerError: 'Сервер не отвечает',
+  LoginError: 'Почта или пароль неверный',
+  UserWithEmailExist: 'Пользователь с таким email уже существует',
   Length: (min, max) => `Поле должно быть от ${min} до ${max} символов`,
 };
