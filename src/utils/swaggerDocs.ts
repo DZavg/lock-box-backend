062efe1483a213ff6dcf569388f215b33187103f
Данил <d.zavgorodnij@meta.studio>
2023-09-30 19:28:00 +0700

    feat(swagger): add bearer auth

--- src/utils/swaggerDocs.ts
+++ src/utils/swaggerDocs.ts
@@ -4,4 +4,5 @@ export default new DocumentBuilder()
   .setTitle('The best swagger in the world for the PM service')
   .setDescription('The pm API description')
   .setVersion('1.0')
+  .addBearerAuth()
   .build();
