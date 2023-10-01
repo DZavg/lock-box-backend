0248fa27b5083ce2879d31fbafc1640a77cba44f
Данил <d.zavgorodnij@meta.studio>
2023-10-01 12:47:42 +0700

    feat(utils): use decoration for check field on unique in create-user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 import { errorMessage } from '@/utils/errorMessage';
+import { IsUnique } from '@/utils/isUnique';
+import { User } from '@/users/entities/user.entity';
 
 export class CreateUserDto {
   @IsNotEmpty({ message: errorMessage.IsNotEmpty })
   @IsString({ message: errorMessage.IsString })
   @IsEmail({}, { message: errorMessage.IsEmail })
+  @IsUnique(User, { message: errorMessage.UserWithEmailExist })
   email: string;
 
   @IsNotEmpty({ message: errorMessage.IsNotEmpty })
