0a4fd2f1f9b3b0968f4912f5460904e75d5642ba
Данил <d.zavgorodnij@meta.studio>
2023-10-12 18:53:27 +0700

    feat(user): add check on password at login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
 
     const equalsPass = await compareString(
       loginDto.password || '',
-      user.password,
+      user?.password,
     );
 
     if (!equalsPass) {
