354a506eeb752b048f3d2b14ab4ca433ada42cb8
Данил <d.zavgorodnij@meta.studio>
2023-10-23 18:26:23 +0700

    feat(e2e tests): add runInBand flag

--- test/run.sh
+++ test/run.sh
@@ -2,6 +2,6 @@ set -e
 
 docker compose up -d db-test
 
-jest --config ./test/jest-e2e.json || docker compose down db-test
+jest --runInBand --config ./test/jest-e2e.json || docker compose down db-test
 
 docker compose down db-test
