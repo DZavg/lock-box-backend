78ec9841371b2575bfbc41e37bcb351078143b93
Данил <d.zavgorodnij@meta.studio>
2023-11-21 20:32:00 +0700

    feat(hash): add func for hash by sha256

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,10 +1,15 @@
 import { compare, hash } from 'bcrypt';
+import * as sha256 from 'crypto-js/sha256';
 
 export const hashStringByBcrypt = async (
   string: string,
   salt: number,
 ): Promise<string> => {
   return await hash(string, salt);
+};
+
+export const hashStringBySha256 = (string: string) => {
+  return sha256(string).toString();
 };
 
 export const compareStringWithHashByBcrypt = async (
