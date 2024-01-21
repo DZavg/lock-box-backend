eefd612ce2ab867a2d719b85e77554a61c39fb50
Данил <d.zavgorodnij@meta.studio>
2024-01-21 19:54:30 +0700

    feat(role): create role guard

---
+++ src/users/role.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import Role from '@/users/role.enum';
+import { ROLES_KEY } from '@/users/roles.decorator';
+import { UnauthorizedException } from '@/utils/exception/unauthorizedException';
+import { ForbiddenException } from '@/utils/exception/forbiddenException';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const includesRequiredRole = requiredRoles.some(
+      (role) => user.roles?.includes(role),
+    );
+
+    if (!includesRequiredRole) {
+      throw new ForbiddenException();
+    }
+
+    return true;
+  }
+}
