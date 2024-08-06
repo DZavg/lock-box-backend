39433e0c260dd92da78d97d734e5b6f5005a8578
DZavg <danilkaqaz123@gmail.com>
2024-08-06 22:16:31 +0300

    feat(create-access.dto): add validation on length

--- src/accesses/dto/create-access.dto.ts
+++ src/accesses/dto/create-access.dto.ts
@@ -1,18 +1,22 @@
 import { IsNotEmpty } from '@/utils/decorators/validation/isNotEmpty';
 import { IsString } from '@/utils/decorators/validation/isString';
 import { AccessTypeDto } from '@/accesses/dto/access-type.dto';
+import { Length } from '@/utils/decorators/validation/length';
 
 export class CreateAccessDto {
   @IsNotEmpty()
   @IsString()
+  @Length(1, 30)
   origin: string;
 
   @IsNotEmpty()
   @IsString()
+  @Length(1, 30)
   login: string;
 
   @IsNotEmpty()
   @IsString()
+  @Length(1, 30)
   password: string;
 
   @IsNotEmpty()
