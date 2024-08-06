4a923cb0e3ab1c848f29bc099f89f6df8babc5ac
DZavg <danilkaqaz123@gmail.com>
2024-08-06 22:16:37 +0300

    feat(create-project.dto): add validation on length

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,12 +1,15 @@
 import { IsNotEmpty } from '@/utils/decorators/validation/isNotEmpty';
 import { IsString } from '@/utils/decorators/validation/isString';
+import { Length } from '@/utils/decorators/validation/length';
 
 export class CreateProjectDto {
   @IsNotEmpty()
   @IsString()
+  @Length(1, 30)
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @Length(1, 30)
   domain: string;
 }
