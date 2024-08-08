6877a9bb9f1333ec91586e89e66e79c271715711
DZavg <danilkaqaz123@gmail.com>
2024-08-08 22:53:03 +0300

    feat(swaggerDocs): update title and description

--- src/utils/swaggerDocs.ts
+++ src/utils/swaggerDocs.ts
@@ -1,8 +1,8 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export default new DocumentBuilder()
-  .setTitle('The best swagger in the world for the PM service')
-  .setDescription('The pm API description')
+  .setTitle('The best swagger in the world for the Lock Box service')
+  .setDescription('The Lock Box API description')
   .setVersion('1.0')
   .addBearerAuth()
   .build();
